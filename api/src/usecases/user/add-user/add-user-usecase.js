34a2e904f0b6ca270f3db2e878d8a4e10acedfc6
William Koller <williamkoller30@gmail.com>
2024-02-26 10:42:36 -0300

    chore: initial commit with nestjs

--- api/src/usecases/user/add-user/add-user-usecase.js
+++
@@ -1,9 +0,0 @@
-import UserModel from '../../../domain/models/user/user-model.js';
-
-class AddUserUseCase {
-  create(userProps) {
-    return UserModel.create(userProps);
-  }
-}
-
-export default AddUserUseCase;
